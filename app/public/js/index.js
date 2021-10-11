d17d5f638d12fc7b0f1027cbc96e81c7995925ef
kaifoste <fosterk202@gmail.com>
2021-10-11 12:04:12 -0400

    workingdb

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,11 +1,12 @@
 
 const SomeApp = {
     data() {
-        return {
-          students: [],
-          selectedStudent: null,
-          offers: []
-        }
+      return {
+        result: {},
+        list: [5,6,7,8],
+        message: "Waiting ...",
+        books: []
+      }
     },
     computed: {
         prettyBirthday() {
@@ -25,52 +26,21 @@ const SomeApp = {
             console.error(error);
         });
         },
-        prettyData(d) {
-            return dayjs(d)
-            .format('D MMM YYYY')
-        },
-        prettyDollar(n) {
-            const d = new Intl.NumberFormat("en-US").format(n);
-            return "$ " + d;
-        },
-        selectStudent(s) {
-            if (s == this.selectedStudent) {
-                return;
-            }
-            this.selectedStudent = s;
-            this.offers = [];
-            this.fetchOfferData(this.selectedStudent);
-        },
-        fetchStudentData() {
+        fetchBookData() {
             fetch('/api/book/')
             .then( response => response.json() )
             .then( (responseJson) => {
                 console.log(responseJson);
-                this.students = responseJson;
+                this.books = responseJson;
             })
             .catch( (err) => {
                 console.error(err);
             })
-        },
-        fetchOfferData(s) {
-            console.log("Fetching offer data for ", s);
-            fetch('/api/offer/?book=' + s.id)
-            .then( response => response.json() )
-            .then( (responseJson) => {
-                console.log(responseJson);
-                this.offers = responseJson;
-            })
-            .catch( (err) => {
-                console.error(err);
-            })
-            .catch( (error) => {
-                console.error(error);
-            });
         }
     },
     created() {
         this.fetchUserData();
-        this.fetchStudentData();
+        this.fetchBookData()
     }
 
   }
