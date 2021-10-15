971e492c1cd5cb50efba4f8477191464f306930e
kaifoste <fosterk202@gmail.com>
2021-10-15 10:33:59 -0400

    Workingform

--- app/public/api/book/create.php
+++ app/public/api/book/create.php
@@ -53,4 +53,4 @@ $pk = $db->lastInsertId();
 // Here, instead of giving output, I'm redirecting to the SELECT API,
 // just in case the data changed by entering it
 header('HTTP/1.1 303 See Other');
-header('Location: ../offer/?student=' . $_POST['title']);
+header('Location: ../book/?book=' . $_POST['title']);
