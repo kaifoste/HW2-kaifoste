8ef92446a4a31309a9340f072d90b0129c109b6f
kaifoste <fosterk202@gmail.com>
2021-10-25 12:54:59 -0400

    add,edit,delete (not working edit)

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -6,7 +6,8 @@ const SomeApp = {
         list: [5,6,7,8],
         message: "Waiting ...",
         books: [],
-        bookForm: {}
+        bookForm: {},
+        selectedBook: null
       }
     },
     computed: {
@@ -38,6 +39,36 @@ const SomeApp = {
                 console.error(err);
             })
         },
+        selectBook(s) {
+            if (s == this.selectedBook) {
+                return;
+            }
+            this.selectedBook = s;
+            this.books = [];
+            this.fetchBookData(this.selectedBook);
+        },
+        postEditBook(evt) {
+            // this.offerForm.studentId = this.selectedStudent.id;        
+            console.log("Updating!", this.bookForm);
+            // alert("Posting!");
+    
+            fetch('api/book/update.php', {
+                method:'POST',
+                body: JSON.stringify(this.bookForm),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.books = json;
+                
+                // reset the form
+                this.resetBookForm();
+              });
+        },
         postNewBook(evt) {
             // this.offerForm.studentId = this.selectedStudent.id;        
             console.log("Posting:", this.bookForm);
@@ -59,6 +90,36 @@ const SomeApp = {
                 // reset the form
                 this.bookForm = {};
               });
+        },
+        postDeleteBook(o) {
+            if (!confirm("Are you sure you want to delete "+o.title+"?")) {
+                return;
+            }
+            
+            fetch('api/book/delete.php', {
+                method:'POST',
+                body: JSON.stringify(o),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.books = json;
+    
+                // reset the form
+                this.resetBookForm();
+              });
+        },
+        selectBookToEdit(o) {
+            this.selectedBook = o;
+            this.bookForm = Object.assign({}, this.selectedBook);
+        },
+        resetBookForm() {
+            this.selectedBook = null;
+            this.bookForm = {};
         }
     },
     created() {
